16a0097eae6d80d41b96b6f956839b26632d455c
unknown <isaac4learn@gmail.com>
2020-02-09 18:27:32 -0500

    Updates

---
+++ precache-manifest.8cdf500001589f47450430099a8c523e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "774efa17db75811fa503a497a0d34d94",
+    "url": "/crwn-clothing/index.html"
+  },
+  {
+    "revision": "3e424a569abbac94482a",
+    "url": "/crwn-clothing/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "e0460477da80b175f7d3",
+    "url": "/crwn-clothing/static/js/2.537fc122.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/crwn-clothing/static/js/2.537fc122.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e424a569abbac94482a",
+    "url": "/crwn-clothing/static/js/main.89aee3a2.chunk.js"
+  },
+  {
+    "revision": "0476b8ec577c2ae33b1e",
+    "url": "/crwn-clothing/static/js/runtime-main.844656b4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/crwn-clothing/static/media/logo.5d5d9eef.svg"
+  }
+]);
